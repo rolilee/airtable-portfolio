45d4c5d0b6c435c81374421c66d17bc64efdc616
rolilee <140334766+rolilee@users.noreply.github.com>
2023-07-29 20:46:36 +0200

    change 5

--- pages/index.js
+++ pages/index.js
@@ -4,50 +4,59 @@ export async function getServerSideProps() {
   const res = await fetch(`${process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : 'http://localhost:3000'}/api/users`);
   if (!res.ok) {
     console.error("Failed to fetch users", res.status);
+    // Handle the error as appropriate for your application.
+    // For example, you might return default props, or throw an error to fail the whole page.
     return { props: { users: [] } };
   }
   const data = await res.json();
   return { props: { users: data } };
 }
 
+
 export default function Home({ users }) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 font-voces bg-center bg-no-repeat bg-cover" 
-     style={{ backgroundImage: `url('/images/background.jpg')` }}
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 font-voces" 
+     style={{ backgroundImage: `url('/images/background.jpg')`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}
     >
       {users.slice(2, 3).map((user) => (
         <div key={user.id} className="flex flex-col items-center justify-center space-y-4">
           {user.fields.Photo &&
-            <div className="w-64 h-64 bg-center bg-cover rounded-lg my-8 sm:w-48 sm:h-48 md:w-64 md:h-64" style={{ backgroundImage: `url(${user.fields.Photo[0].url})`, marginTop: '50px' }}></div>}
+            <div className="w-64 h-64 bg-center bg-cover rounded-lg my-8" style={{ backgroundImage: `url(${user.fields.Photo[0].url})`, marginTop: '50px' }}></div>}
           {user.fields['Full name'] &&
-            <h1 className="text-xl sm:text-2xl md:text-3xl font-bold font-open-sans text-center mt-4 mb-8 tracking-wide text-white">
+            <h1 className="text-3xl font-bold font-open-sans text-center mt-4 mb-8 tracking-wide" style={{ color: '#FFFAFA' }}>
               {user.fields['Full name']}
             </h1>}
           {user.fields['Position and company name'] &&
-            <p className="text-xl sm:text-xl md:text-2xl leading-6 tracking-wider text-white mb-12">{user.fields['Position and company name']}</p>}
-          <div className={`flex flex-wrap justify-center sm:justify-between w-96 mb-4`}>
+            <p className="text-2xl leading-6 tracking-wider" style={{ marginBottom: '50px', color: '#FFFAFA' }}>{user.fields['Position and company name']}</p>}
+          <div className={`flex ${user.fields.WhatsApp ? 'justify-between' : 'justify-center'} w-96 mb-4`} style={{ marginBottom: '50px' }}>
             {user.fields['Phone number'] && 
-              <a href={`tel:${user.fields['Phone number']}`} className="m-3">
-                <img src="/phone.svg" className="h-11 w-11" alt="Phone" />
+              <a href={`tel:${user.fields['Phone number']}`} className={`${user.fields.WhatsApp ? '' : 'mr-3'}`}>
+                <div className="inline-block">
+                  <img src="/phone.svg" className="h-11 w-11" alt="Phone" />
+                </div>
               </a>}
             {user.fields.Email && 
-              <a href={`mailto:${user.fields.Email}`} className="m-3">
-                <img src="/email.svg" className="h-11 w-11" alt="Email" />
+              <a href={`mailto:${user.fields.Email}`} className={`${user.fields.WhatsApp ? '' : 'ml-20'}`}>
+                <div className="inline-block">
+                  <img src="/email.svg" className="h-11 w-11" alt="Email" />
+                </div>
               </a>}
             {user.fields.WhatsApp &&
-              <a href={`https://wa.me/${user.fields.WhatsApp}`} className="m-3">
-                <img src="/whatsapp.svg" className="h-11 w-11" alt="WhatsApp" />
+              <a href={`https://wa.me/${user.fields.WhatsApp}`}>
+                <div className="inline-block">
+                  <img src="/whatsapp.svg" className="h-11 w-11" alt="WhatsApp" />
+                </div>
               </a>}
           </div>
           {user.fields.Website &&
-            <button className="bg-white rounded-lg px-4 py-2 text-base sm:text-lg md:text-xl mb-4 text-gray-700 w-full sm:w-auto" onClick={() => window.open(user.fields.Website, '_blank')}><strong>Website</strong></button>}
+            <button style={{ backgroundColor: '#FFFAFA', borderRadius: '10px', padding: '20px 300px', fontSize: '19px', marginBottom: '20px', color: '#656539' }} onClick={() => window.open(user.fields.Website, '_blank')}><strong>Website</strong></button>}
           {user.fields.LinkedIn &&
-            <button className="bg-white rounded-lg px-4 py-2 text-base sm:text-lg md:text-xl mb-4 text-gray-700 w-full sm:w-auto" onClick={() => window.open(user.fields.LinkedIn, '_blank')}><strong>LinkedIn</strong></button>}
+            <button style={{ backgroundColor: '#FFFAFA', borderRadius: '10px', padding: '20px 297px', fontSize: '19px', marginBottom: '20px', color: '#656539' }} onClick={() => window.open(user.fields.LinkedIn, '_blank')}><strong>LinkedIn</strong></button>}
           {user.fields.Facebook &&
-            <button className="bg-white rounded-lg px-4 py-2 text-base sm:text-lg md:text-xl mb-4 text-gray-700 w-full sm:w-auto" onClick={() => window.open(user.fields.Facebook, '_blank')}><strong>Facebook</strong></button>}
+            <button style={{ backgroundColor: '#FFFAFA', borderRadius: '10px', padding: '20px 294px', fontSize: '19px', marginBottom: '20px', color: '#656539' }} onClick={() => window.open(user.fields.Facebook, '_blank')}><strong>Facebook</strong></button>}
           {user.fields.Instagram &&
-            <button className="bg-white rounded-lg px-4 py-2 text-base sm:text-lg md:text-xl mb-8 text-gray-700 w-full sm:w-auto" onClick={() => window.open(user.fields.Instagram, '_blank')}><strong>Instagram</strong></button>}
-          <button className="bg-yellow-500 rounded-full px-4 py-2 text-base sm:text-lg md:text-xl mb-12 text-white w-full sm:w-auto" onClick={() => window.open('https://www.netink.ch/', '_blank')}><strong>Open in NetInk App</strong></button>
+            <button style={{ backgroundColor: '#FFFAFA', borderRadius: '10px', padding: '20px 292px', fontSize: '19px', marginBottom: '70px', color: '#656539' }} onClick={() => window.open(user.fields.Instagram, '_blank')}><strong>Instagram</strong></button>}
+          <button style={{ backgroundColor: '#9B9B43', borderRadius: '50px', padding: '20px 50px', fontSize: '19px', marginBottom: '50px', color: '#FFFAFA' }} onClick={() => window.open('https://www.netink.ch/', '_blank')}><strong>Open in NetInk App</strong></button>
         </div>
       ))}
     </div>
